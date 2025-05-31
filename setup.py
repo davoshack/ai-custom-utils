948f1923e5511369ad28a9a7d1f838271fa9c8ac
Juan D Hernandez G <juandavos941@gmail.com>
2025-05-30 22:48:26 -0500

    Update setup file

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     author="Davos Code",
     author_email="jdhernandez@azzertai.com",
     license="MIT",
-    packages=["ai_custom_utils", "ai_custom_utils.agentic_patterns"],
+    packages=[
+        "ai_custom_utils", 
+        "ai_custom_utils.agentic_patterns.planning_pattern", 
+        "ai_custom_utils.agentic_patterns.multiagent_pattern", 
+        "ai_custom_utils.agentic_patterns.tool_pattern", 
+        "ai_custom_utils.agentic_patterns.reflection_pattern",
+        "ai_custom_utils.agentic_patterns.utils"
+    ],
     zip_safe=False,
 )
