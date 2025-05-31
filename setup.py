06a3696db7969334cd2f9e322fbcadedffe57ae2
Juan D Hernandez G <juandavos941@gmail.com>
2025-05-30 22:42:11 -0500

    Add agentic patterns helpers

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author="Davos Code",
     author_email="jdhernandez@azzertai.com",
     license="MIT",
-    packages=["ai_custom_utils",],
+    packages=["ai_custom_utils", "ai_custom_utils.agentic_patterns"],
     zip_safe=False,
 )
