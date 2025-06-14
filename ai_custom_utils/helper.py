2edb05f4505328d8d2b7f6cfa1050899fdca9c72
Juan D Hernandez G <juandavos941@gmail.com>
2025-06-14 17:46:53 -0500

    Get MLFlow Tracking Uri

--- ai_custom_utils/helper.py
+++ ai_custom_utils/helper.py
@@ -124,6 +124,11 @@ def get_serper_api_key():
     load_env()
     serper_api_key = os.getenv("SERPER_API_KEY")
     return serper_api_key
+
+def get_mlflow_tracking_uri():
+    load_dotenv()
+    mlflow_tracking_uri = os.getenv("DLAI_LOCAL_URL")
+    return mlflow_tracking_uri
     
 
 def print_response(text):
